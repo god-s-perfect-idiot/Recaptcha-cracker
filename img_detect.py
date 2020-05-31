93ff01e477b76a4e4779863009e7b79a9da9db66
Samar <33544311+god-s-perfect-idiot@users.noreply.github.com>
2020-05-31 16:44:52 +0530

    Add files via upload

--- img_detect.py
+++ img_detect.py
@@ -1,19 +1,18 @@
 from keras.applications.resnet50 import ResNet50
+from keras.applications.xception import Xception
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 import os
-
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-with tf.device("/cpu:0"):
-    model = ResNet50(weight='imagenet')
+model = ResNet50(weights='imagenet')
 
-    img_path = 'ima.png'
-    img = image.load_img(img_path, target_size=(130,130))
+img_path = 'ima.png'
+img = image.load_img(img_path, target_size=(224,224))
 
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
-    preds = model.predict(x)
-    print(decode_predictions(preds,top=3)[0])
+preds = model.predict(x)
+print(decode_predictions(preds,top=3)[0])
