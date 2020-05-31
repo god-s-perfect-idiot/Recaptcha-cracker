2b7bb982f658211b4b7c4db94c39b4bf0a0348e4
Samar <33544311+god-s-perfect-idiot@users.noreply.github.com>
2020-05-31 20:12:06 +0530

    Add files via upload

--- get_imgs.py
+++ get_imgs.py
@@ -1,26 +1,29 @@
 import cv2
-
-im = cv2.imread("page.png")
-cropped = im[190:590,90:485]
 #cv2.imshow("a",cropped)
 
+def prepare():
 
-ima = cropped[0:130,0:130]
-imb = cropped[0:130,130:260]
-imc = cropped[0:130,260:390]
-imd = cropped[130:260,0:130]
-ime = cropped[130:260,130:260]
-imf = cropped[130:260,260:390]
-img = cropped[260:390,0:130]
-imh = cropped[260:390,130:260]
-imi = cropped[260:390,260:390]
+    im = cv2.imread("page.png")
+    cropped = im[260:750,110:600]
 
-cv2.imwrite("ima.png",ima)
-cv2.imwrite("imb.png",imb)
-cv2.imwrite("imc.png",imc)
-cv2.imwrite("imd.png",imd)
-cv2.imwrite("ime.png",ime)
-cv2.imwrite("imf.png",imf)
-cv2.imwrite("img.png",img)
-cv2.imwrite("imh.png",imh)
-cv2.imwrite("imi.png",imi)
+    ima = cropped[0:165,0:165]
+    imb = cropped[0:165,165:330]
+    imc = cropped[0:165,330:490]
+    imd = cropped[165:330,0:165]
+    ime = cropped[165:330,165:330]
+    imf = cropped[165:330,330:490]
+    img = cropped[330:490,0:165]
+    imh = cropped[330:490,165:330]
+    imi = cropped[330:490,330:490]
+
+    cv2.imwrite("ima.png",ima)
+    cv2.imwrite("imb.png",imb)
+    cv2.imwrite("imc.png",imc)
+    cv2.imwrite("imd.png",imd)
+    cv2.imwrite("ime.png",ime)
+    cv2.imwrite("imf.png",imf)
+    cv2.imwrite("img.png",img)
+    cv2.imwrite("imh.png",imh)
+    cv2.imwrite("imi.png",imi)
+
+prepare()
--- img_detect.py
+++ img_detect.py
@@ -4,15 +4,32 @@ from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 import os
+from PIL import Image, ImageEnhance
+from skimage.color import rgb2gray
+from skimage import io
+
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 model = ResNet50(weights='imagenet')
 
-img_path = 'ima.png'
-img = image.load_img(img_path, target_size=(224,224))
+def detectall():
+    s = ['a','b','c','d','e','f','g','h','i']
+    for i in range(9):
+        img_path = 'im'+s[i]+'.png'
+        pil_img = Image.open(img_path)
+        enhancer = ImageEnhance.Sharpness(pil_img)
+        enhanced_im = enhancer.enhance(10.0)
 
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+        img = image.load_img(img_path, interpolation="nearest", target_size=(224,224))
 
-preds = model.predict(x)
-print(decode_predictions(preds,top=3)[0])
+
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        preds = model.predict(x)
+        predn = decode_predictions(preds,top=3)[0]
+
+        for j in range(3):
+            print(str(i)+". "+str(predn[j][1]))
+
+detectall()
